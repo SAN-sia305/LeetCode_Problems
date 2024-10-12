14cf4ada6aa235845896564fadc66dfb0e3ead05
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2024-10-12 18:46:07 +0530

    Time: 93 ms (56.14%), Space: 73.2 MB (70.32%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+#include<unordered_set>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      unordered_set<int> seen;
+      for(int i : nums){
+            if(seen.find(i) != seen.end()){
+                return true;
+            }
+            seen.insert(i);
+          }
+      return false;
+
+      }
+    
+};
