5aca838f83217819794d75f19e938dda397f984d
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2024-11-23 21:04:40 +0530

    Time: 2 ms (5.93%) | Memory: 7.6 MB (59.43%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+class Solution {
+public:
+    char repeatedCharacter(string s){
+        vector<int> freq(26,0);
+        for(char c:s){ 
+            freq[c-'a']++;
+            if(freq[c-'a'] == 2){ 
+                return c;
+                break; 
+            }
+        }
+    return -1;    
+    }
+};
