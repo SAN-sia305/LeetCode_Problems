78c7cbcd4134b51ecc39f32d4e7d0fe7fa34e844
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2024-12-03 11:24:01 +0530

    Time: 628 ms (33.53%) | Memory: 379.1 MB (18.19%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        //BFS WITH ADJACENCY LIST (while loop)
-        unordered_map<int, vector<int>> adjacenCList;
-        for(auto edge : edges){
-            int u = edge[0], v = edge[1];
-            adjacenCList[u].push_back(v);
-            adjacenCList[v].push_back(u);
-        }
-
-        queue<int> q;
-        q.push(source);
-
-        vector<bool> visited(n, false);
+    bool dsf(int n, vector<bool>& visited ,unordered_map<int, vector<int>>& adjacenCList, int source, int destination){
+        if(source == destination) return true;
+        
         visited[source] = true;
-
-        while(!q.empty()){
-            int current = q.front();
-            q.pop();
-
-            if(current==destination) return true;
-            for(int neighbour : adjacenCList[current]){
-                if(!visited[neighbour]){
-                    q.push(neighbour);
-                    visited[neighbour] = true;
-                }
+        for(int neighbour : adjacenCList[source]){
+            if(!visited[neighbour]) {
+               if(dsf(n, visited , adjacenCList, neighbour, destination)){return true;}
             }
+            
         }
-    return false;    
+   
+    return false;
+    }
+
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        //DFS WITH ADJACENCY MATRIX (recursion)
+        unordered_map<int, vector<int>> adjacenCList;
+       for(auto edge : edges){
+         int u = edge[0],v = edge[1];
+         adjacenCList[u].push_back(v);
+         adjacenCList[v].push_back(u);
+       }
+
+       vector<bool> visited(n,false);
+       visited[source] = true;
+
+       return dsf(n, visited , adjacenCList, source, destination);
     }
 };
