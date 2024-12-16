c66b366582928ca0daccfe536cf98e0e051e5aca
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2024-12-16 11:45:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.33%) - LeetSync

--- 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
         int time = arrivalTime+delayedTime;
-        if(time > 24){
-            return time%24;
-        }
-        else if(time < 24){
-            return time;
-        }
-        else{
-            return 0;
-        }
+        return time%24;
     }
 };
