7b349daa5357987b2f7d83b5ff8d27319f5580e6
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2024-12-17 10:49:53 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (5.97%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,16 +4,20 @@ public:
        int size = nums.size();
        if (size <= 1 || k == 0) return;
        vector<int> swaped(size);
-       int idx = 0;
-       k = k % size;
-       for(int i = size-k; i < size; i++){
-          swaped[idx++] = nums[i];
-          cout<<nums[i]<<endl;
-       }
-       for(int i = 0; i < size-k; i++){
-          swaped[idx++] = nums[i];
-          cout<<nums[i]<<endl;
-        }
+    //    int idx = 0;
+    //    k = k % size;
+    //    for(int i = size-k; i < size; i++){
+    //       swaped[idx++] = nums[i];
+    //       cout<<nums[i]<<endl;
+    //    }
+    //    for(int i = 0; i < size-k; i++){
+    //       swaped[idx++] = nums[i];
+    //       cout<<nums[i]<<endl;
+    //     }
+
+    for(int i = 0; i < size;i++){
+        swaped[(i+k)%size] = nums[i];
+    }
     nums = swaped;
     }
 };
