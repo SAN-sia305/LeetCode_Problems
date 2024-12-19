2480a72dd75cd2fc0aac0a3966c923b0463fa1e8
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2024-12-19 10:52:01 +0530

    Time: 23 ms (83.41%) | Memory: 73.4 MB (53.44%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,15 +3,20 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-      unordered_set<int> seen;
-      for(int i : nums){
-            if(seen.find(i) != seen.end()){
-                return true;
-            }
-            seen.insert(i);
-          }
-      return false;
-
-      }
+    //   unordered_set<int> seen;
+    //   for(int i : nums){
+    //         if(seen.find(i) != seen.end()){
+    //             return true;
+    //         }
+    //         seen.insert(i);
+    //       }
+    //   return false;
+    unordered_map<int,int> checker;
+    for(int i : nums){
+       checker[i]++;
+       if(checker[i] >=2 ){return true; }
+    }
+    return false;
+    }
     
 };
