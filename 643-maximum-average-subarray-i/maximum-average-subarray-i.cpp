08bcaff751d3464e359dda8d9f77798005b199fe
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2026-05-25 14:32:00 +0530

    Time: 0 ms (100.00%) | Memory: 113.7 MB (85.30%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int size = nums.size();
+        int sum = 0;
+        for(int i=0; i<k; i++){
+            sum += nums[i];
+        }
+
+        int maxsum = sum;
+        for(int i = k; i< size; i++){
+            sum = sum-nums[i-k]+nums[i];
+            maxsum = max(maxsum,sum);
+        }
+
+        double avg = double(maxsum)/double(k);
+        return avg;
+    }
+};
