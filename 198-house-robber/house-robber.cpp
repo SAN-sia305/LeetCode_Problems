98bb1240a2c4f47929d34151efb5b464fab168d9
SAN-sia305 <158986423+SAN-sia305@users.noreply.github.com>
2026-05-26 11:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (20.85%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int mav = 0 ,prv = 0;
-        for(int i = 0; i < nums.size(); i++){
-              int temp = max(mav,prv+nums[i]);
-              prv = mav;
-              mav = temp;
+        int n = nums.size();
+        if(n == 0)
+            return 0;
+        if(n==1)
+            return nums[0];
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2;i < n; i++){
+            dp[i] = max(dp[i-1],nums[i]+dp[i-2]);
         }
-        return mav;
+        return dp[n-1];
     }
 };
